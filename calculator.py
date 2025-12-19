01df2347cd358ff5dfa52e2d96a577e1e53c42cc
Auston Ivison <austonivison@users.noreply.github.com>
2025-12-19 16:28:19 -0700

    Feat: Add square root (GitHub Flow: Feature branch)

--- calculator.py
+++ calculator.py
@@ -13,6 +13,9 @@ class Calculator:
     def modulus(self, a, b):
         return a % b
 
+    def square_root(self, a):
+        return a ** 0.5
+
 if __name__ == "__main__":
     calc = Calculator()
     print("1 + 2 =", calc.add(1, 2))
